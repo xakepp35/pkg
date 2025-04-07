c7f54396fd289be15b6bacd5d0426db85aec09ab
sergeyl <sergeyl@anykey.pro>
2025-04-07 15:22:39 +0300

    fix env

--- env/env.go
+++ env/env.go
@@ -7,6 +7,7 @@ import (
 	"time"
 )
 
+// Get returns the environment variable specified by key, or the default value specified by def if empty
 func Get[T any](key string, def T) T {
 	switch tp := (any)(def).(type) {
 	case string:
@@ -48,140 +49,142 @@ func Get[T any](key string, def T) T {
 	}
 }
 
-// Env returns the environment variable specified by key, or the default value specified by def if empty
-func String(key string, def string) string {
-	result := os.Getenv(key)
-	if result == "" {
-		return def
-	}
-	return strings.TrimSpace(result)
-}
-
 const envListSeparator = ","
 
-func Strings(key string, def []string) []string {
-	return strings.Split(Get(key, strings.Join(def, envListSeparator)), envListSeparator)
-}
-
-// IntFromEnv Check environment variable and return the value as int
-func Int(key string, def int) int {
-	envStr := os.Getenv(key)
-	if envStr == "" {
+func String(key string, def string) string {
+	val, ok := os.LookupEnv(key)
+	if !ok || strings.TrimSpace(val) == "" {
 		return def
 	}
-	result, err := strconv.Atoi(envStr)
+	return strings.TrimSpace(val)
+}
+
+func Strings(key string, def []string) []string {
+	val, ok := os.LookupEnv(key)
+	if !ok || val == "" {
+		return def
+	}
+	return strings.Split(val, envListSeparator)
+}
+
+func Int(key string, def int) int {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return def
+	}
+	v, err := strconv.Atoi(val)
 	if err != nil {
 		return def
 	}
-	return result
+	return v
 }
 
 func Int64(key string, def int64) int64 {
-	envStr := os.Getenv(key)
-	if envStr == "" {
+	val, ok := os.LookupEnv(key)
+	if !ok {
 		return def
 	}
-	result, err := strconv.ParseInt(envStr, 10, 64)
+	v, err := strconv.ParseInt(val, 10, 64)
 	if err != nil {
 		return def
 	}
-	return result
+	return v
 }
 
 func Int32(key string, def int32) int32 {
-	envStr := os.Getenv(key)
-	if envStr == "" {
+	val, ok := os.LookupEnv(key)
+	if !ok {
 		return def
 	}
-	result, err := strconv.ParseInt(envStr, 10, 32)
+	v, err := strconv.ParseInt(val, 10, 32)
 	if err != nil {
 		return def
 	}
-	return int32(result)
+	return int32(v)
 }
 
 func Int16(key string, def int16) int16 {
-	envStr := os.Getenv(key)
-	if envStr == "" {
+	val, ok := os.LookupEnv(key)
+	if !ok {
 		return def
 	}
-	result, err := strconv.ParseInt(envStr, 10, 16)
+	v, err := strconv.ParseInt(val, 10, 16)
 	if err != nil {
 		return def
 	}
-	return int16(result)
+	return int16(v)
 }
 
 func Int8(key string, def int8) int8 {
-	envStr := os.Getenv(key)
-	if envStr == "" {
+	val, ok := os.LookupEnv(key)
+	if !ok {
 		return def
 	}
-	result, err := strconv.ParseInt(envStr, 10, 8)
+	v, err := strconv.ParseInt(val, 10, 8)
 	if err != nil {
 		return def
 	}
-	return int8(result)
+	return int8(v)
 }
 
 func Uint(key string, def uint) uint {
-	envStr := os.Getenv(key)
-	if envStr == "" {
+	val, ok := os.LookupEnv(key)
+	if !ok {
 		return def
 	}
-	result, err := strconv.ParseUint(envStr, 10, 64)
+	v, err := strconv.ParseUint(val, 10, 64)
 	if err != nil {
 		return def
 	}
-	return uint(result)
+	return uint(v)
 }
 
 func Uint64(key string, def uint64) uint64 {
-	envStr := os.Getenv(key)
-	if envStr == "" {
+	val, ok := os.LookupEnv(key)
+	if !ok {
 		return def
 	}
-	result, err := strconv.ParseUint(envStr, 10, 64)
+	v, err := strconv.ParseUint(val, 10, 64)
 	if err != nil {
 		return def
 	}
-	return result
+	return v
 }
 
 func Uint32(key string, def uint32) uint32 {
-	envStr := os.Getenv(key)
-	if envStr == "" {
+	val, ok := os.LookupEnv(key)
+	if !ok {
 		return def
 	}
-	result, err := strconv.ParseUint(envStr, 10, 32)
+	v, err := strconv.ParseUint(val, 10, 32)
 	if err != nil {
 		return def
 	}
-	return uint32(result)
+	return uint32(v)
 }
 
 func Uint16(key string, def uint16) uint16 {
-	envStr := os.Getenv(key)
-	if envStr == "" {
+	val, ok := os.LookupEnv(key)
+	if !ok {
 		return def
 	}
-	result, err := strconv.ParseUint(envStr, 10, 16)
+	v, err := strconv.ParseUint(val, 10, 16)
 	if err != nil {
 		return def
 	}
-	return uint16(result)
+	return uint16(v)
 }
 
 func Uint8(key string, def uint8) uint8 {
-	envStr := os.Getenv(key)
-	if envStr == "" {
+	val, ok := os.LookupEnv(key)
+	if !ok {
 		return def
 	}
-	result, err := strconv.ParseUint(envStr, 10, 8)
+	v, err := strconv.ParseUint(val, 10, 8)
 	if err != nil {
 		return def
 	}
-	return uint8(result)
+	return uint8(v)
 }
 
 func Uintptr(key string, def uintptr) uintptr {
@@ -189,58 +192,57 @@ func Uintptr(key string, def uintptr) uintptr {
 }
 
 func Float64(key string, def float64) float64 {
-	envStr := os.Getenv(key)
-	if envStr == "" {
+	val, ok := os.LookupEnv(key)
+	if !ok {
 		return def
 	}
-	result, err := strconv.ParseFloat(envStr, 64)
+	v, err := strconv.ParseFloat(val, 64)
 	if err != nil {
 		return def
 	}
-	return result
+	return v
 }
 
 func Float32(key string, def float32) float32 {
-	envStr := os.Getenv(key)
-	if envStr == "" {
+	val, ok := os.LookupEnv(key)
+	if !ok {
 		return def
 	}
-	result, err := strconv.ParseFloat(envStr, 32)
+	v, err := strconv.ParseFloat(val, 32)
 	if err != nil {
 		return def
 	}
-	return float32(result)
+	return float32(v)
 }
 
-// Bool return the value of env variable as bool
 func Bool(key string, def bool) bool {
-	envStr := os.Getenv(key)
-	if envStr == "" {
+	val, ok := os.LookupEnv(key)
+	if !ok {
 		return def
 	}
-	return strings.ToLower(envStr) == "true"
+	return strings.ToLower(val) == "true"
 }
 
 func Duration(key string, def time.Duration) time.Duration {
-	envStr := os.Getenv(key)
-	if envStr == "" {
+	val, ok := os.LookupEnv(key)
+	if !ok {
 		return def
 	}
-	result, err := time.ParseDuration(envStr)
+	v, err := time.ParseDuration(val)
 	if err != nil {
 		return def
 	}
-	return result
+	return v
 }
 
 func Time(key string, def time.Time) time.Time {
-	envStr := os.Getenv(key)
-	if envStr == "" {
+	val, ok := os.LookupEnv(key)
+	if !ok {
 		return def
 	}
-	result, err := time.Parse(time.RFC3339Nano, envStr)
+	v, err := time.Parse(time.RFC3339Nano, val)
 	if err != nil {
 		return def
 	}
-	return result
+	return v
 }
