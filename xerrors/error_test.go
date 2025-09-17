d67b4fe7e17e27849e15aac20d619a27f66520fa
ssturch <108745158+ssturch@users.noreply.github.com>
2025-09-17 11:49:16 +0300

    Добавлена собственная имплементация метода Is для кастомной ошибки (#46)
    * in process
    
    * in process
    
    * add tests / fixes
    
    * add is method for custom err

--- xerrors/error_test.go
+++ xerrors/error_test.go
@@ -30,6 +30,41 @@ func TestIs(t *testing.T) {
 
 		require.True(t, errors.Is(err, sql.ErrNoRows))
 	})
+
+	t.Run("err by proto", func(t *testing.T) {
+		err := Err(sql.ErrNoRows).Proto(codes.NotFound)
+
+		require.True(t, errors.Is(err, sql.ErrNoRows))
+	})
+
+	t.Run("err by proto msg", func(t *testing.T) {
+		err := Err(sql.ErrNoRows).MsgProto(codes.NotFound, "not found")
+
+		require.True(t, errors.Is(err, sql.ErrNoRows))
+	})
+}
+
+func TestNewProto(t *testing.T) {
+	t.Run("check unwrapped error", func(t *testing.T) {
+		err := NewProto(codes.AlreadyExists, errors.New("foo"), "bar")
+		require.Equal(t, "bar: foo", err.Error())
+	})
+	t.Run("check code by error", func(t *testing.T) {
+		err := NewProto(codes.AlreadyExists, errors.New("foo"), "bar")
+		status, ok := status.FromError(err)
+		if !ok {
+			t.Error("got absent grpc status")
+			return
+		}
+		if status.Code() != codes.AlreadyExists {
+			t.Errorf("got grpc status code: %v, want %v", status.Code().String(), codes.AlreadyExists.String())
+			return
+		}
+		if status.Message() != "bar" {
+			t.Errorf("got grpc status message: %v, want bar", status.Message())
+			return
+		}
+	})
 }
 
 func TestNewProto(t *testing.T) {
